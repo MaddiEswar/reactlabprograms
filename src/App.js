597e557dc54dab90fb69b97ddb957feb33e6bee5
maddi eswar <2002eswar@gmail.com>
2022-10-11 20:13:18 +0530

    ArrayofObjects

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React from 'react';
 import Counter from './components/Counter';
 import TextColor from './components/ChangingTextColor';
-
+import ArrayofObjects from './components/ArrayofObjects';
 
 function App() {
   return (
     <div className="App">
       {/* <Counter /> */}
-      <TextColor />
+      {/* <TextColor /> */}
+      <ArrayofObjects />
     </div>
   );
 }
