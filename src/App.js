ddd0bde14b21fd75945154593999d93b695ca167
maddi eswar <2002eswar@gmail.com>
2022-10-11 21:11:25 +0530

    Digitalclock

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import Counter from './components/Counter';
 import TextColor from './components/ChangingTextColor';
 import ArrayofObjects from './components/ArrayofObjects';
+import DigitalClock from './components/DigitalClock';
 
 function App() {
   return (
     <div className="App">
       {/* <Counter /> */}
       {/* <TextColor /> */}
-      <ArrayofObjects />
+      {/* <ArrayofObjects /> */}
+      <DigitalClock />
     </div>
   );
 }
