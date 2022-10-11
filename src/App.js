ff5800fa47867ccd5e9c37aad570affee77fa28f
maddi eswar <2002eswar@gmail.com>
2022-10-11 19:34:48 +0530

    changingtextcolor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react';
 import Counter from './components/Counter';
+import TextColor from './components/ChangingTextColor';
+
 
 function App() {
   return (
     <div className="App">
       {/* <Counter /> */}
-      
+      <TextColor />
     </div>
   );
 }
